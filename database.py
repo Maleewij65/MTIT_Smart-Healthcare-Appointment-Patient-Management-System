e59af678e3a6cdb28f89d63676df6eb8dcf6533d
Maleesha Wijerathne <153164960+Maleewij65@users.noreply.github.com>
2026-03-28 16:17:57 +0530

    Update database.py
    Changed db to access everyone

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 import os
+import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGO_URI"))
+client = MongoClient(
+    os.getenv("MONGO_URI"),
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=5000
+)
 
 db = client["patient_db"]
 patient_collection = db["patients"]
